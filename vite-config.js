b330f0b148acf99e9aabeed481c5d618ff8f58ab
KinyattaAndaiStar004 <23214@student.embuni.ac.ke>
2025-12-08 13:20:30 +0300

    Created a file called Vite config.js for redirection purporses

---
+++ vite-config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  appType: 'mpa', // MPA = Multi-Page Application (not SPA)
+  build: {
+    rollupOptions: {
+      input: {
+        login: 'login.html',
+        signup: 'signup.html',
+        index: 'index.html',
+        playerDashboard: 'player/player-dashboard.html',
+        captainDashboard: 'captain/captain-dashboard.html',
+        coachDashboard: 'coach/coach-dashboard.html'
+      }
+    }
+  }
+})
